a37215982fa42ef8fa8a4e813023b91d72c718df
Ragima Bagirova <ragima2412@gmail.com>
2023-10-24 07:32:56 +0400

    change Background Image and video for hero banner

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Video from "../../assets/video/ocean-action.mp4";
+import Video from "../../assets/video/mountains.mp4";
 import "./VideoPlayer";
 
 const VideoPlayer = () => {
   return (
     <div className="video-player" >
-      <video  width="1280" height="720" autoPlay loop preload="auto">
+      <video  width="1280" height="720" loop autoPlay muted >
         <source src={Video} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
